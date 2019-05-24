8d0c6eca70af731d9f94ab1b0b6e3e9f8986f160
Sum-li <43328116+Sum-li@users.noreply.github.com>
2019-05-24 19:54:06 +0800

    C++

---
+++ demo1/cmake-build-debug/CMakeFiles/demo1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/a_Sum/C++program/demo1/main.cpp" "D:/a_Sum/C++program/demo1/cmake-build-debug/CMakeFiles/demo1.dir/main.cpp.obj"
+  "D:/a_Sum/C++program/demo1/test1.cpp" "D:/a_Sum/C++program/demo1/cmake-build-debug/CMakeFiles/demo1.dir/test1.cpp.obj"
+  "D:/a_Sum/C++program/demo1/test2.cpp" "D:/a_Sum/C++program/demo1/cmake-build-debug/CMakeFiles/demo1.dir/test2.cpp.obj"
+  "D:/a_Sum/C++program/demo1/test3.cpp" "D:/a_Sum/C++program/demo1/cmake-build-debug/CMakeFiles/demo1.dir/test3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demo1/cmake-build-debug/CMakeFiles/demo1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo1.dir/main.cpp.obj"
+  "CMakeFiles/demo1.dir/test1.cpp.obj"
+  "CMakeFiles/demo1.dir/test2.cpp.obj"
+  "CMakeFiles/demo1.dir/test3.cpp.obj"
+  "demo1.pdb"
+  "demo1.exe"
+  "demo1.exe.manifest"
+  "libdemo1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
